039dd57a831fe7c5b4f9b90a0e4db4ab409e1c84
Najla Haura <najlahaura27@gmail.com>
2026-01-30 02:40:40 +0700

    Seccond Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gojek/pages/home_page.dart';
+import 'package:gojek/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,13 +9,62 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gojek',
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: green2,
+          elevation: 0,
+          toolbarHeight: 115 - 44,
+          title: Container(
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: green1,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 14,
+                    vertical: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Text(
+                    'Beranda',
+                    style: semibold14.copyWith(color: green1),
+                  ),
+                ),
+
+                ...['Promo', 'Pesanan', 'Chat']
+                    .map(
+                      (title) => Flexible(
+                        fit: FlexFit.loose,
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 14,
+                            vertical: 12,
+                          ),
+                          child: Center(
+                            child: Text(
+                              title,
+                              style: semibold14.copyWith(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
